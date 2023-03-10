ae12159d669b64465f460d79e2e94f782781de6e
Alan Bui <alanb3350@gmail.com>
2023-03-09 22:19:24 -0500

    added puzzle1

--- chessjudge/script.js
+++ chessjudge/script.js
@@ -2,12 +2,34 @@
 // https://github.com/jhlywa/chess.js
 import { Chess } from '/chessjudge/chess.js'
 
+const moves = new Map()
 
-var board = null
-var game = new Chess()
-var $status = $('#status')
-var $fen = $('#fen')
-var $pgn = $('#pgn')
+let curMove = 0
+let numMoves = 0
+
+async function getData() { 
+  //sending a GET request to get a bunch of information about the user
+  const URL = '/chessjudge/puzzles.json';
+  
+  const Params = {
+      method: 'GET'
+  }
+  
+  return await fetch(URL, Params)
+      .then(res => res.json()) //returns the JSON
+      .catch(err => alert(err)) //throws error
+}
+
+getData().then(
+  function(value) {
+    console.log(value)
+    const FENs = value["puzzle1"]["FENS"]
+    for (let i=0; i < FENs.length; i++){
+      moves.set(i, FENs[i])
+      numMoves ++
+    }
+  }
+);
 
 function onDragStart (source, piece, position, orientation) {
   // do not pick up pieces if the game is over
@@ -30,6 +52,27 @@ function onDrop (source, target) {
 
   // illegal move
   if (move === null) return 'snapback'
+
+  if (!(game.fen() === moves.get(curMove+1))){
+    console.log("SAD")
+    board = Chessboard('myBoard', config)
+    game = new Chess('3r2k1/3r1ppp/8/8/3R4/6P1/7P/3R2K1 b - - 0 1')
+  }
+
+  else{
+    curMove += 2
+    if (curMove != numMoves){
+      config.position = moves.get(curMove)
+      console.log("YAY")
+    }
+    else{
+      config.position = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1'
+      alert("YOU WIN")
+    }
+
+    board = Chessboard('myBoard', config)
+    game = new Chess(moves.get(curMove))
+  }
 
   updateStatus()
 }
@@ -67,7 +110,7 @@ function updateStatus () {
       status += ', ' + moveColor + ' is in check'
     }
   }
-
+  
   $status.html(status)
   $fen.html(game.fen())
   $pgn.html(game.pgn())
@@ -75,12 +118,16 @@ function updateStatus () {
 
 var config = {
   draggable: true,
-  position: 'start',
   onDragStart: onDragStart,
   onDrop: onDrop,
-  onSnapEnd: onSnapEnd
+  onSnapEnd: onSnapEnd,
+  position: '3r2k1/3r1ppp/8/8/3R4/6P1/7P/3R2K1 b - - 0 1'
 }
-board = Chessboard('myBoard', config)
+
+var board = Chessboard('myBoard', config)
+var game = new Chess('3r2k1/3r1ppp/8/8/3R4/6P1/7P/3R2K1 b - - 0 1')
+var $status = $('#status')
+var $fen = $('#fen')
+var $pgn = $('#pgn')
 
 updateStatus()
-console.log("HELOO")
